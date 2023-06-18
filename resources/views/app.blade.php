2bbaf87bd700cd1e22ebc31d6d9f201067dc4be6
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-18 11:21:55 -0500

    tercer commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="theme-color" content="#6777ef"/>
     <link rel="shortcut icon" href="{{ asset('afrohuila_1.svg') }}">
     
