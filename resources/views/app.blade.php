d7440c2c9f7e2e6e6c8908696f9ef124e3b8ff7a
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-18 14:53:27 -0500

    reasignar rutas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    
     <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
     <meta name="theme-color" content="#6777ef"/>
-    <link rel="shortcut icon" href="{{ asset('afrohuila_1.svg') }}">
+    <link rel="shortcut icon" href="{{ asset('/afrohuila_1.svg') }}">
     
-    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('/apple-touch-icon.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
     @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
