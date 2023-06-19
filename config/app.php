b75377188946854d6884ea70590d89caea66265c
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-19 10:42:28 -0500

    app2

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://example-app-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
