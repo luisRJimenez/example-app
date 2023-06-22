89baa4d244ccf734971d5fce77492982dc0e78c2
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-21 19:12:05 -0500

    handel inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -17,7 +17,11 @@ class HandleInertiaRequests extends Middleware
     /**
      * Determine the current asset version.
      */
-    
+    public function version(Request $request)
+    {
+        return parent::version($request);
+    }
+
 
     /**
      * Define the props that are shared by default.
