fcb6bd399f6811995a512b6de29aa3c77b8c8270
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-22 18:58:53 -0500

    dasboard3

--- resources/js/Pages/Dashboard.svelte
+++ resources/js/Pages/Dashboard.svelte
@@ -8,14 +8,16 @@
     <title>Dashboard</title>
 </svelte:head>
 
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div
-            class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
-        >
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                You're logged in!
+<AuthenticatedLayout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div
+                class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+            >
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    You're logged in!
+                </div>
             </div>
         </div>
     </div>
-</div>
+</AuthenticatedLayout>
