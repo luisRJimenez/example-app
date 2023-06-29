d58af32e786ca178c11f0fec4fcc1a85e5355bf1
LuisRJimenez <luis.jimenezb@cun.edu.co>
2023-06-28 21:40:08 -0500

    update role

--- resources/js/Pages/Roles/index.svelte
+++ resources/js/Pages/Roles/index.svelte
@@ -66,7 +66,7 @@
 
   const updaterol = () => {
     console.log($form.id)
-    $form.put(` roles/${$form.id}`),  {
+    $form.put(`roles/${$form.id}`),  {
       preserveState: false,
       onSuccess: () => {
               Toast.fire({
